7333d08c2c428825c81b36ccd10554584b154626
DDA <dda@DDAs-MacBook-Air.local>
2021-07-01 15:51:18 +0300

    add methods: "patient_status_up", "patient_status_down"

--- tdd_trening/patients/patients.py
+++ tdd_trening/patients/patients.py
@@ -1,3 +1,13 @@
 class PatientsRepository():
     def __init__(self, patients_db):
         self._patients_db = patients_db
+
+    def patient_status_up(self, patient_id):
+        patient_index = patient_id - 1
+        status_code = self._patients_db[patient_index]
+        self._patients_db[patient_index] = status_code + 1
+
+    def patient_status_down(self, patient_id):
+        patient_index = patient_id - 1
+        status_code = self._patients_db[patient_index]
+        self._patients_db[patient_index] = status_code - 1
--- tdd_trening/patients/test_patients.py
+++ tdd_trening/patients/test_patients.py
@@ -5,3 +5,15 @@ def test_create_patients_repository():
     patients_db = [1, 2, 3]
     patients = PatientsRepository(patients_db)
     assert patients._patients_db == [1, 2, 3]
+
+
+def test_patient_status_up():
+    patients = PatientsRepository([1, 1, 1])
+    patients.patient_status_up(2)
+    assert patients._patients_db == [1, 2, 1]
+
+
+def test_patient_status_down():
+    patients = PatientsRepository([1, 1, 1])
+    patients.patient_status_down(2)
+    assert patients._patients_db == [1, 0, 1]
